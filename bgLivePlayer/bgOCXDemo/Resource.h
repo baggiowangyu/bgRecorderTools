799ae18df96cac97c4577e7fdaa1c0f28aca61b5
baggiowangyu <baggiowangyu@gmail.com>
2018-03-05 08:51:14 +0800

    OCX增加界面

--- bgLivePlayer/bgOCXDemo/Resource.h
+++ bgLivePlayer/bgOCXDemo/Resource.h
@@ -2,19 +2,22 @@
 // Microsoft Visual C++ generated include file.
 // Used by bgOCXDemo.rc
 //
+#define IDS_BGOCXDEMO                   1
+#define IDB_BGOCXDEMO                   1
+#define IDS_BGOCXDEMO_PPG               2
+#define IDR_HTML_OCXPLAYERSCREENDLG     103
+#define IDS_BGOCXDEMO_PPG_CAPTION       200
+#define IDD_PROPPAGE_BGOCXDEMO          200
+#define IDC_EDIT1                       201
+#define IDD_DLG_PLAYER_SCREEN           201
 
-#define IDS_BGOCXDEMO               1
-#define IDS_BGOCXDEMO_PPG           2
-
-#define IDS_BGOCXDEMO_PPG_CAPTION   200
-
-#define IDD_PROPPAGE_BGOCXDEMO      200
-
-
-#define IDB_BGOCXDEMO               1
-
-
-#define _APS_NEXT_RESOURCE_VALUE        201
-#define _APS_NEXT_CONTROL_VALUE         201
-#define _APS_NEXT_SYMED_VALUE           101
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        202
 #define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         202
+#define _APS_NEXT_SYMED_VALUE           104
+#endif
+#endif
--- bgLivePlayer/bgOCXDemo/bgOCXDemo_i.c
+++ bgLivePlayer/bgOCXDemo/bgOCXDemo_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Wed Feb 28 15:19:25 2018
+/* at Fri Mar 02 15:00:30 2018
  */
 /* Compiler settings for .\bgOCXDemo.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
